13a237144a355f660cb1c702798e5e822be16491
Andrea Ciceri <andrea.ciceri@autistici.org>
2024-12-14 17:29:39 +0100

    Problem 2 part 1
    Co-authored-by: AngeloChecked <angi99552@gmail.com>

--- aoc.nix
+++ aoc.nix
@@ -5,9 +5,23 @@ lib: let
     smallerSorted = quicksort (lib.lists.filter (a: a <= x) xs);
     biggerSorted = quicksort (lib.lists.filter (a: a > x) xs);
   in smallerSorted ++ [x] ++ biggerSorted;
+  
   abs = n: if n >= 0 then n else -n;
+  
+  isReportSafe = report: let
+    _isSafe = report: builtins.foldl'
+    (acc: level: let
+      diff = level - acc.previousLevel;
+      isInRange = diff >= -3 && diff <= 3 && diff != 0;
+    in rec {
+      inc = level > acc.previousLevel;
+      previousLevel = level;
+      safe = if acc.safe == false || (!isInRange) || (acc.inc != null && inc != acc.inc) then false else true;
+    })
+      {inc = null; previousLevel = lib.lists.head report; safe = true;} (lib.lists.tail report);
+  in (_isSafe report).safe;
 in {
-  inherit quicksort;
+  inherit quicksort isReportSafe;
   
   aoc1a = input: let
     lines = lib.strings.splitString "\n" input;
@@ -32,5 +46,15 @@ in {
     occurrences = builtins.foldl' (acc: item: acc // {"${builtins.toString item}" = (acc."${builtins.toString item}" or 0) + 1;}) {} listRight;
     similiratyScore = builtins.foldl' (acc: item: acc + item * (occurrences."${builtins.toString item}" or 0)) 0 listLeft;
     result = similiratyScore;
+  in result;
+
+  aoc2a = input: let
+    lines = lib.strings.splitString "\n" input;
+    lines' = lib.lists.take (lib.lists.length lines - 1) lines;
+    splitLine = lib.strings.splitString " ";
+    ls = lib.lists.map splitLine lines';
+    reports = lib.lists.map (lib.lists.map builtins.fromJSON) ls;
+    countSafe = builtins.foldl' (acc: report: acc + (if isReportSafe report then 1 else 0)) 0 reports;
+    result = countSafe;
   in result;
 }
--- test.nix
+++ test.nix
@@ -12,6 +12,22 @@ in runTests {
     expr = aoc.aoc1b (lib.readFile ./aoc1.txt);
     expected = 24316233;
   };
+  testAoc2a = {
+    expr = aoc.aoc2a (lib.readFile ./aoc2.txt);
+    expected = 24316233;
+  };
+  testIsReportSafe = {
+    expr = aoc.isReportSafe [1 2 4 5 6];
+    expected = true;
+  };
+  testIsReportSafeIsUnsafeIfIncreasingMoreThanThree = {
+    expr = aoc.isReportSafe [1 4 8 9];
+    expected = false;
+  };
+  testIsReportSafeNotMonotonic = {
+    expr = aoc.isReportSafe [1 1];
+    expected = false;
+  };
   testQuickSort = {
     expr = aoc.quicksort [2 6 23 6 8 9];
     expected = [2 6 6 8 9 23];
